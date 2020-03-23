740a1269ea1723b912ec9164a1220b59ecb6f88b
ba1ake <blakegabb@gmail.com>
2020-03-23 22:36:19 +0000

    added fishin and eating to the game and fixed some sleeping problems

--- main.py
+++ main.py
@@ -2,29 +2,30 @@ from noise import pnoise2
 import sys
 import time
 import random
+# to do
+#fix sleeping, fixed i think, i might even remove it
+
+
+
+
+
+
+
 def spawn(spawnrange):
   return(random.randint((spawnrange*-1 ), spawnrange))
+  #this gens the spawn area to a range set.
 #this is the world genartor using perlin noise
-world = []
-"""
-def clear():
-  #this is to clear the line
-  print("                                   ")
-  sys.stdout.write(u"\u001b[1A")
-  sys.stdout.write(u"\u001b[1000D")
-"""
 def clean():
   #this whole part is to wipe the screen to make it look good
         sys.stdout.write(u"\u001b[1A")
         sys.stdout.write(u"\u001b[1A")
         sys.stdout.write(u"\u001b[1000D")
-        print("                                   ")
-        print("                                   ")
+        print("""                                                                       """)
+        print("""                                                                       """)
         sys.stdout.write(u"\u001b[1A")
         sys.stdout.write(u"\u001b[1A")
         sys.stdout.write(u"\u001b[1000D")
-
-energy = 100
+energy = 50
 def worldgen(x,y,seed):
   #this is some code i got in order toit get vaules via pnoise2, it is a simple algorwith that helps and such
  tile = (pnoise2(x/seed,y/seed,2))
@@ -34,7 +35,7 @@ def worldgen(x,y,seed):
  #vector = 1 #this is for testing purposes
  biome = (pnoise2(x/(seed*10),y/(seed*10),2))
  if biome > 0.3:#acrapellgo
-  if tile > 0.10:
+  if tile > 0.20:
     return("+")
   else:
     print(".")
@@ -55,11 +56,11 @@ def worldgen(x,y,seed):
  elif biome > -0.3: #hilly boi biome
 
    if tile > .10:
-     return("=")
+     return("=") #edges of moutain
    elif tile > -0.1:
-     return ("/")
+     return ("/") #black
    elif tile > -0.3:
-     return ("|")
+     return ("|") # black
    else:
      return(".")
  else: # just ocean
@@ -67,20 +68,49 @@ def worldgen(x,y,seed):
     return ("/") # this should try to resemble some ice bergs or something
   else:
    return("-")
-
+def upgrades(xp,foodcap,movedis, energyrestore):
+  clean()
+  print("you have", xp,"xp, it costs 25 to upgrade")
+  time.sleep(2)
+  print()
+  clean()
+  option = input("press and enter 1, for max food, 2 for speed, or 3 for energy from food ")
+  time.sleep(1)
+  print()
+  clean()
+  if xp > 25:
+    if option == "1":
+      print("max food cap has been upgraded to", foodcap + 1)
+      time.sleep(1)
+      clean()
+      foodcap += 1
+      xp -=25
+    elif option == "2":
+      print("you can now run", movedis + 2,"blocks")
+      time.sleep(1)
+      clean()
+      movedis +=2
+      xp -= 25
+    elif option == "3":
+      energyrestore += 1
+      print("food now restores", energyrestore, "energy")
+      time.sleep(1)
+      clean()
+      xp -= 25
+  else:
+    print()("you dont have enough xp to upgrade a skill, move around and do stuff.")
+    time.sleep(1)
+    clean()
+      
+  
 #### MAIN LOOP ####
-
 #seed effects the sises of the islands and such
-spawnrange = 1 # this is how random-ness works for this program as the vaule x and y determins the seed so by having a massive range the randomness is increseed
+spawnrange = 999999 # this is how random-ness works for this program as the vaule x and y determins the seed so by having a massive range the randomness is increseed
 #this makes it so that the player has the same map but has a diffrent spawn region each time
 seed = 15
 x = spawn(spawnrange)
 y = spawn(spawnrange)
-
-
-
-
-# this just gets the render sise
+# this just gets the render sise and makes sure its in the paramters of spawn range and not an odd number
 rendersise = input("choose render sise, and please have it as an even number ")
 try:
   rendersise = int(rendersise)
@@ -93,6 +123,8 @@ else:
   rendersise += 1
   print("number was not even, adding one to it to fix this")
   time.sleep(1)
+
+# just some stuff
 maxy = rendersise
 maxx = rendersise
 posx = x
@@ -100,23 +132,29 @@ posy = y
 xdone = 0
 ydone = 0
 spawnrange = 1
-
+food = 1
+scrap = 0
+foodcap = 5
+movedis = 10
+energyrestore = 10
+xp = 9999
+#the main bit
 while True:
- sys.stdout.write("\033[0;0f")
- #posx -= 1 #this is for testing this moves to left
- #posy += 1
+ sys.stdout.write("\033[0;0f")#puts the curser at top of screen
  x = posx
  y = posy
  ydone = 0
  xdone = 1
  #rendering
  while True:
-   #this is all the vaules for the rending color,   
+   #this is all the vaules for the rending color, 
   land = worldgen(x,y,seed)
-  if ydone == 0:
-    sys.stdout.write(u"\u001b[41m  ")#red
+  if ydone == (0.5 * rendersise) and xdone == (0.5 * rendersise):
+    sys.stdout.write(u"\u001b[41m  ")
+  elif ydone == 0:
+    sys.stdout.write(u"\u001b[41m  ")#red for border
   elif ydone == (maxy-1):
-    sys.stdout.write(u"\u001b[41m  ")#red
+    sys.stdout.write(u"\u001b[41m  ")#red for border
   elif xdone == (maxx):
     sys.stdout.write(u"\u001b[41m  ")#red
   elif land == "+":
@@ -133,20 +171,19 @@ while True:
   elif land == "x":
     sys.stdout.write(u"\u001b[43;1m  ") # yellow
   elif land == "H":
-    sys.stdout.write(u"\u001b[43m  ") #diffrent shade of yellow but i dont think it works in repl.it
-    
+    sys.stdout.write(u"\u001b[43m  ") #diffrent shade of yellow but i dont think it works in repl.it  
   else:
     sys.stdout.write(u"\u001b[44m  ")
-  #this is when libne hits the 30th pixal
-  if xdone == (maxx): #= 30
-    #drops down to the bigeiing of the nest line so that the sising code works
+  #this is when line hits the 30th pixal
+  if xdone == (maxx): # goes to next line
+    #drops down to the bigeiing of the nest line so that the render sising code works
     sys.stdout.write(u"\u001b[1000D")
     sys.stdout.write(u"\u001b[1B")
     x = posx
     xdone = 0
     ydone += 1
     y += 1
-  if ydone == (maxy):
+  if ydone == (maxy): # this is when the whole thing has rendered and contains options like moveing, sleeping, maybe eating at some point
     sys.stdout.write(u"\u000b")
     # brings the cursor to the start.
     #finsihs when this fully renders(there seems to be a glitch with the render)
@@ -155,9 +192,9 @@ while True:
     if action == "move": # this is the module for getting the player to move
 
       #clear()
-      while True:
-       if energy == 0:
-         print("to tired to move, you have died.")
+      while True: #this is a while true loop as i can use it to auto exit a secton and skip the other parts which a normal thign wont be able to do
+       if energy <= 0:
+         print("you are to tired to move, you have died due to the lack of energy to move your legs. While your body is alive it wont be for long. You can already hear the wovles stalking around.")
          exit()
        else:
           ree = 1
@@ -165,13 +202,14 @@ while True:
         distance = int(input("now far "))
         break
        except:
-        print("not a str")
+        print("not a int")
         time.sleep(1)
         clean()
-      if distance > 10:
-        print("to far moving by ten")
+      if distance > movedis: #this makes sure the player doesnt go to far
+        print("to far moving by", movedis)
         time.sleep(1)
-        distance = 10
+        distance = movedis
+        
         print("")
         clean()
       move = input("where? ")
@@ -186,13 +224,15 @@ while True:
       clean()
       clean()
       energy -= distance
-    elif action == "sleep":
+      
+      #this makes them only able to wal a curtain distance 
+    elif action == "sleep": #see if can sleep
      backupx = posx
      backupy = posy
-     posx = (rendersise/2) + posx
-     posy = (rendersise / 2) + posy
+     x = (rendersise/2) + posx
+     y = (rendersise / 2) + posy
      if worldgen(x,y,seed) == "+":
-      sleep = 50
+      energy = 50
       print("you feel better")
       time.sleep(1)
       clean()
@@ -202,7 +242,54 @@ while True:
        clean()
      posx = backupx
      posy = backupy
-
+    elif action == "fish":
+     food_or_scrap = random.randint(1,4)
+     if food_or_scrap == 4:
+       print("you didnt catch a fish but got some scrap")
+       scrap +=1
+       time.sleep(1)
+       clean()
+       break
+     if food > foodcap:
+       print("you have to many fish,")
+       time.sleep(1)
+       clean()
+       break
+     backupx = posx
+     backupy = posy
+     x = (rendersise/2) + posx
+     y = (rendersise / 2) + posy
+     if worldgen(x,y,seed) == "." or worldgen(x,y,seed) == "-":
+      print("you fished and caught a fish, congrats")
+      time.sleep(1)
+      xp += 1
+      clean()
+      food +=1
+      posy = backupy
+      posx = backupx
+     else:
+       print("fun fact, fishing with out water is somewhat hard to do")
+       time.sleep(2)
+       clean()
+       xp += 1
+     posx = backupx
+     posy = backupy
+    elif action == "eat": #this is if the player has food and if so lets them reafina some energy back from it but no more than 50
+      if food > 0:
+        print("you have eaten some food, you have regained some energy")
+        energy += energyrestore
+        time.sleep(1)
+        food -= 1
+        clean()
+        if energy > 50:
+          energy = 50
+      else:
+        print("you know, you can really eat food thats not there. Right?")
+        time.sleep(1)
+        clean()
+    elif action == "xp":
+      upgrades(xp,foodcap,movedis,energyrestore)
+      print("you now have ",xp, " xp remaining")# the resone for this being a fucntion is i might change how it is done so by having this as a function will make it eair to move it
     else:
       print("please retry")
       time.sleep(1)
